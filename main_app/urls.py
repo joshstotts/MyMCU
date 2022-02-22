dfce400485a236ec9af7746100017dfadb59353b
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-02-22 16:51:53 -0600

    nav bar working

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('allMovies/', views.allMovies, name='allMovies'),
+    path('favorites/', views.favorites, name='favorites'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def allMovies(request):
+    return render(request, 'allMovies.html')
+
+def favorites(request):
+    return render(request, 'favorites/index.html')
+
